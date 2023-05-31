24bb0fb43c4f1abd9029bc381b798b2115a0dd4d
WTFender <wtfender.cs@gmail.com>
2023-05-31 16:05:19 -0500

    show release notes

--- src/entry/background.ts
+++ src/entry/background.ts
@@ -5,7 +5,6 @@ import { createFirefoxContainer } from '../utils';
 extension.log('background:init');
 
 // setup listeners to create firefox containers
-
 function listenConsole() {
   if (!extension.config.browser.webRequest.onBeforeRequest.hasListener(createFirefoxContainer)) {
     extension.log('background:listenConsole');
@@ -23,32 +22,54 @@ function listenConsole() {
   } else { extension.log('background:listenConsole:listenerExists'); }
 }
 
-// setup listeners on message / settings change
-extension.config.browser.runtime.onMessage.addListener((msg: ExtensionMessage) => {
-  extension.checkPermissions().then((permissions) => {
-    if (msg.action === 'enableFirefoxContainers') {
-      extension.log('background:enableFirefoxContainers');
-      if (permissions.containers) {
-        listenConsole();
-      }
-    } else if (msg.action === 'disableFirefoxContainers') {
-      extension.log('background:disableFirefoxContainers');
-      if (permissions.containers) {
-        extension.config.browser.webRequest.onBeforeRequest.removeListener(createFirefoxContainer);
-      }
-    }
-  });
-});
-
-// startup listeners
-extension.checkPermissions().then((permissions) => {
-  // permissions
-  if (permissions.containers) {
-    extension.loadData().then((data: ExtensionData) => {
-      // settings
-      if (data.settings.firefoxContainers) {
-        listenConsole();
-      }
+// show release notes on install & update
+extension.config.browser.runtime.onInstalled.addListener((details) => {
+  const manifest = extension.config.browser.runtime.getManifest();
+  const currentVersion = manifest.version;
+  extension.log(`currentVersion: ${currentVersion}`);
+  if (details.reason === 'install') {
+    extension.config.browser.tabs.create({
+      url: `https://github.com/WTFender/aws-sso-extender/releases/tag/v${currentVersion}`,
     });
+  } else if (details.reason === 'update') {
+    if (details.previousVersion !== currentVersion) {
+      extension.log(`previousVersion: ${details.previousVersion}`);
+      extension.config.browser.tabs.create({
+        url: `https://github.com/WTFender/aws-sso-extender/releases/tag/v${currentVersion}`,
+      });
+    }
   }
 });
+
+if (extension.platform === 'firefox') {
+  // setup listeners on message / settings change
+  extension.config.browser.runtime.onMessage.addListener((msg: ExtensionMessage) => {
+    extension.checkPermissions().then((permissions) => {
+      if (msg.action === 'enableFirefoxContainers') {
+        extension.log('background:enableFirefoxContainers');
+        if (permissions.containers) {
+          listenConsole();
+        }
+      } else if (msg.action === 'disableFirefoxContainers') {
+        extension.log('background:disableFirefoxContainers');
+        if (permissions.containers) {
+          // eslint-disable-next-line vue/max-len
+          extension.config.browser.webRequest.onBeforeRequest.removeListener(createFirefoxContainer);
+        }
+      }
+    });
+  });
+
+  // startup listeners
+  extension.checkPermissions().then((permissions) => {
+  // permissions
+    if (permissions.containers) {
+      extension.loadData().then((data: ExtensionData) => {
+      // settings
+        if (data.settings.firefoxContainers) {
+          listenConsole();
+        }
+      });
+    }
+  });
+}
