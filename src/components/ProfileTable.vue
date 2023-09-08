68c39f2d5ac3f14ed44bac04d929a6af0d20b0a6
WTFender <wtfender.cs@gmail.com>
2023-09-08 18:14:19 -0500

    resize img, font, spacing

--- src/components/ProfileTable.vue
+++ src/components/ProfileTable.vue
@@ -52,12 +52,19 @@
             v-model:visible="colorPickerVisible"
             :style="{ width: '50vw' }"
           >
-            <ColorPicker v-if="colorPickerVisible" v-model="activeProfile.profile.custom!.color" :inline="true" />
+            <ColorPicker
+              v-if="colorPickerVisible"
+              v-model="activeProfile.profile.custom!.color"
+              :inline="true"
+            />
           </PDialog>
         </div>
         <div
-          v-if="activeProfile.applicationName === 'AWS Account' && (!permissions.console || !permissions.signin)"
-          style="text-align: center; padding-top: 20px;"
+          v-if="
+            activeProfile.applicationName === 'AWS Account'
+              && (!permissions.console || !permissions.signin)
+          "
+          style="text-align: center; padding-top: 20px"
         >
           <PrimeButton
             size="small"
@@ -67,8 +74,9 @@
             style="margin-top: 5px"
             @click="requestPermissions()"
           />
-          <p style="margin-top: 10px;">
-            In order to customize the AWS console and assume IAM roles, this extension requires additional permissions.
+          <p style="margin-top: 10px">
+            In order to customize the AWS console and assume IAM roles, this extension
+            requires additional permissions.
           </p>
         </div>
         <div v-else-if="activeProfile.applicationName === 'AWS Account'">
@@ -92,9 +100,7 @@
               :disabled="true"
             />
           </div>
-          <div
-            style="margin-bottom: 10px"
-          >
+          <div style="margin-bottom: 10px">
             <small id="profile-label-help">AWS Console Preview</small>
             <InputText
               id="consolePreview"
@@ -138,29 +144,51 @@
   </PDialog>
 
   <!--- profile table filters--->
-  <div v-if="tableEditor" style="background-color: #f3f5fb; border-bottom: 3px solid #dee2e6; padding-top: 10px; padding-bottom: 10px;">
+  <div
+    v-if="tableEditor"
+    style="
+      background-color: #f3f5fb;
+      border-bottom: 3px solid #dee2e6;
+      padding-top: 10px;
+      padding-bottom: 10px;
+    "
+  >
     <PrimeButton
       size="small"
       :icon="newTableSettings.showIcon ? 'pi pi-check-circle' : 'pi pi-circle'"
       class="filter-button"
-      :class="newTableSettings.showIcon !== false ? 'p-button-primary' : 'p-button-secondary'"
+      :class="
+        newTableSettings.showIcon !== false ? 'p-button-primary' : 'p-button-secondary'
+      "
       label="Icons"
-      @click="newTableSettings.showIcon = !newTableSettings.showIcon; $emit('updateTableSettings', newTableSettings)"
+      @click="
+        newTableSettings.showIcon = !newTableSettings.showIcon;
+        $emit('updateTableSettings', newTableSettings);
+      "
     />
     <PrimeButton
       size="small"
       :icon="newTableSettings.showIamRoles ? 'pi pi-check-circle' : 'pi pi-circle'"
       class="filter-button"
-      :class="newTableSettings.showIamRoles !== false ? 'p-button-primary' : 'p-button-secondary'"
+      :class="
+        newTableSettings.showIamRoles !== false
+          ? 'p-button-primary'
+          : 'p-button-secondary'
+      "
       label="IAM Roles"
-      @click="newTableSettings.showIamRoles = !newTableSettings.showIamRoles; $emit('updateTableSettings', newTableSettings)"
+      @click="
+        newTableSettings.showIamRoles = !newTableSettings.showIamRoles;
+        $emit('updateTableSettings', newTableSettings);
+      "
     />
     <br v-if="!newTableSettings.showIamRoles || !newTableSettings.showIcon" />
     <PrimeButton
       size="small"
       :icon="sortAppIcon"
       class="filter-button"
-      :class="newTableSettings.sortApp !== false ? 'p-button-primary' : 'p-button-secondary'"
+      :class="
+        newTableSettings.sortApp !== false ? 'p-button-primary' : 'p-button-secondary'
+      "
       label="Account"
       @click="sortByApp()"
     />
@@ -168,7 +196,9 @@
       size="small"
       :icon="sortProfileIcon"
       class="filter-button"
-      :class="newTableSettings.sortProfile !== false ? 'p-button-primary' : 'p-button-secondary'"
+      :class="
+        newTableSettings.sortProfile !== false ? 'p-button-primary' : 'p-button-secondary'
+      "
       label="Profile"
       @click="sortByProfile()"
     />
@@ -177,73 +207,78 @@
       size="small"
       :icon="newTableSettings.sortCustom ? 'pi pi-sort-alt' : 'pi pi-sort-alt-slash'"
       class="filter-button"
-      :class="newTableSettings.sortCustom !== false ? 'p-button-primary' : 'p-button-secondary'"
+      :class="
+        newTableSettings.sortCustom !== false ? 'p-button-primary' : 'p-button-secondary'
+      "
       label="Custom"
     />
   </div>
 
   <!-- header -->
   <div v-if="tableEditor" class="profile">
-    <div class="pi" style="color: transparent; width: 16px;" />
+    <div class="pi" style="color: transparent; width: 16px" />
     <div
       v-if="newTableSettings.showIcon"
       class="profile-field nav"
-      style="width: 100px; vertical-align: middle; padding-left: 0px !important;"
+      style="width: 35px; vertical-align: middle; padding-left: 0px !important"
     />
-    <div
-      class="profile-field nav table-header"
-      :style="{ width: '120px' }"
-    >
+    <div class="profile-field nav table-header" :style="{ width: columnWidth }">
       Account
     </div>
-    <div
-      class="profile-field nav table-header"
-      :style="{ width: '150px' }"
-    >
+    <div class="profile-field nav table-header" :style="{ width: columnWidth }">
       Profile
     </div>
     <div
       v-if="newTableSettings.showIamRoles"
       class="profile-field nav table-header"
-      style="width: 150px;"
+      :style="{ width: '25%' }"
     >
       IAM Roles
     </div>
   </div>
 
   <!--- profile table --->
-  <div v-sortable="{ disabled: !tableEditor, options: { group: 'name', animation: 250, easing: 'cubic-bezier(1, 0, 0, 1)' } }" @end="sortByCustom">
+  <div
+    v-sortable="{
+      disabled: !tableEditor,
+      options: { group: 'name', animation: 250, easing: 'cubic-bezier(1, 0, 0, 1)' },
+    }"
+    @end="sortByCustom"
+  >
     <div
       v-for="profile in sortedProfiles"
       :key="`${profile.id}-${profile.profile.id}`"
       class="profile"
+      style="vertical-align: middle; text-align: left"
     >
-      <div
-        class="pi"
-        :class="{
-          'pi-star-fill': profile.profile.custom?.favorite,
-          'pi-star': !profile.profile.custom?.favorite,
-        }"
-        @click="fave(profile)"
-      />
       <img
         v-if="newTableSettings.showIcon"
         :alt="profile.name"
         :src="profile.icon"
         class="profile-field nav"
-        style="width: 100px; vertical-align: middle; padding-left: 0px !important;"
+        width="100"
+        height="35"
+        style="width: 35px; object-fit: cover; padding-left: 0px"
         @click="!tableEditor ? navSelectedProfile(profile) : editProfile(profile)"
       />
       <div
         class="profile-field nav"
-        :style="{ width: '120px' }"
+        :style="{ width: columnWidth }"
         @click="!tableEditor ? navSelectedProfile(profile) : editProfile(profile)"
       >
         <div v-if="profile.applicationName === 'AWS Account'">
-          <p style="margin: 0px" class="truncate app-title" :title="profile.searchMetadata!.AccountName">
+          <p
+            style="margin: 0px"
+            class="truncate app-title"
+            :title="profile.searchMetadata!.AccountName"
+          >
             <b>{{ newTableSettings.sortApp === 'asc' || newTableSettings.sortApp === 'desc' ? profile.searchMetadata!.AccountName : profile.searchMetadata!.AccountId }}</b>
           </p>
-          <p style="margin: 0px" class="truncate app-subtitle" :title="profile.searchMetadata!.AccountId">
+          <p
+            style="margin: 0px"
+            class="truncate app-subtitle"
+            :title="profile.searchMetadata!.AccountId"
+          >
             {{ newTableSettings.sortApp === 'asc' || newTableSettings.sortApp === 'desc' ? profile.searchMetadata!.AccountId : profile.searchMetadata!.AccountName }}
           </p>
         </div>
@@ -255,7 +290,7 @@
       </div>
       <div
         class="profile-field nav"
-        :style="{ width: '150px' }"
+        :style="{ width: columnWidth }"
         @click="!tableEditor ? navSelectedProfile(profile) : editProfile(profile)"
       >
         <PBadge
@@ -263,33 +298,46 @@
           :value="profile.profile.custom?.label || profile.profile.name"
           :title="profile.profile.custom?.label || profile.profile.name"
           class="truncate"
-          :style="{ width: '150px', 'background-color': profile.profile.custom?.color ? `#${profile.profile.custom?.color}` : `#${user.custom.colorDefault}` }"
+          :style="{
+            verticalAlign: 'middle',
+            width: '100%',
+            'background-color': profile.profile.custom?.color
+              ? `#${profile.profile.custom?.color}`
+              : `#${user.custom.colorDefault}`,
+          }"
         />
-        <br v-else>
+        <br v-else />
       </div>
       <div
         v-if="newTableSettings.showIamRoles && profile.profile.custom?.iamRoles.length! > 0"
         class="profile-field nav"
-        style="width: 150px;"
+        :style="{ width: '25%' }"
       >
         <PBadge
           v-for="(role, idx) in profile.profile.custom?.iamRoles"
           :key="idx"
           :value="role.label || role.roleName"
           class="role-link truncate"
-          :style="{ width: '150px', 'background-color': `#${role.color}` }"
+          :style="{ width: '100%', 'background-color': `#${role.color}`, verticalAlign: 'middle' }"
           :title="role.label || role.roleName"
           @click="!tableEditor ? assumeIamRole(role, profile) : editProfile(profile)"
         />
       </div>
+      <div
+        style="float: right; margin-top: 5px;"
+        class="pi"
+        :class="{
+          'pi-star-fill': profile.profile.custom?.favorite,
+          'pi-star': !profile.profile.custom?.favorite,
+        }"
+        @click="fave(profile)"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  AppData, IamRole, UserData,
-} from '../types';
+import { AppData, IamRole, UserData } from '../types';
 import { getFontColor, waitForElement } from '../utils';
 
 export default {
@@ -375,6 +423,15 @@ export default {
     };
   },
   computed: {
+    columnWidth() {
+      if (!this.newTableSettings.showIcon && !this.newTableSettings.showIamRoles) {
+        return '45%';
+      }
+      if (!this.newTableSettings.showIamRoles) {
+        return '40%';
+      }
+      return '30%';
+    },
     tableSettingsChanged() {
       return JSON.stringify(this.tableSettings) !== JSON.stringify(this.newTableSettings);
     },
@@ -389,16 +446,24 @@ export default {
         } else {
           profile.sortName = profile.profile.custom?.label || profile.name;
         }
-        if (profile.name.toLowerCase().includes(this.search.toLowerCase())
-          || profile.profile.name.toLowerCase().includes(this.search.toLowerCase())) {
+        if (
+          profile.name.toLowerCase().includes(this.search.toLowerCase())
+          || profile.profile.name.toLowerCase().includes(this.search.toLowerCase())
+        ) {
           profiles.push(profile);
         }
       });
       // sort app name
-      if (this.newTableSettings.sortApp === 'desc' || this.newTableSettings.sortApp === 'descNum') {
+      if (
+        this.newTableSettings.sortApp === 'desc'
+        || this.newTableSettings.sortApp === 'descNum'
+      ) {
         return profiles.sort((a, b) => a.sortName!.localeCompare(b.sortName!));
       }
-      if (this.newTableSettings.sortApp === 'asc' || this.newTableSettings.sortApp === 'ascNum') {
+      if (
+        this.newTableSettings.sortApp === 'asc'
+        || this.newTableSettings.sortApp === 'ascNum'
+      ) {
         return profiles.sort((a, b) => b.sortName!.localeCompare(a.sortName!));
       }
       // sort profile name
@@ -437,7 +502,9 @@ export default {
   watch: {
     loaded: {
       handler(v) {
-        if (v) { this.newTableSettings = JSON.parse(JSON.stringify(this.settings.tableSettings)); }
+        if (v) {
+          this.newTableSettings = JSON.parse(JSON.stringify(this.settings.tableSettings));
+        }
       },
     },
     editorVisible: {
@@ -457,11 +524,14 @@ export default {
     sortIcon(sort) {
       if (sort === 'desc') {
         return 'pi pi-sort-alpha-down';
-      } if (sort === 'asc') {
+      }
+      if (sort === 'asc') {
         return 'pi pi-sort-alpha-down-alt';
-      } if (sort === 'descNum') {
+      }
+      if (sort === 'descNum') {
         return 'pi pi-sort-numeric-down';
-      } if (sort === 'ascNum') {
+      }
+      if (sort === 'ascNum') {
         return 'pi pi-sort-numeric-down-alt';
       }
       return 'pi pi-sort-alt-slash';
@@ -480,7 +550,10 @@ export default {
       } else if (this.newTableSettings.sortApp === 'ascNum') {
         this.newTableSettings.sortApp = false;
       }
-      this.$emit('updateTableSettings', { profileEditor: this.editorVisible, ...this.newTableSettings });
+      this.$emit('updateTableSettings', {
+        profileEditor: this.editorVisible,
+        ...this.newTableSettings,
+      });
     },
     sortByProfile() {
       this.newTableSettings.sortCustom = false;
@@ -492,7 +565,10 @@ export default {
       } else if (this.newTableSettings.sortProfile === 'asc') {
         this.newTableSettings.sortProfile = false;
       }
-      this.$emit('updateTableSettings', { profileEditor: this.editorVisible, ...this.newTableSettings });
+      this.$emit('updateTableSettings', {
+        profileEditor: this.editorVisible,
+        ...this.newTableSettings,
+      });
     },
     sortByCustom(event) {
       const customProfileIds: string[] = [];
@@ -507,7 +583,10 @@ export default {
       // eslint-disable-next-line vue/no-mutating-props
       this.user.appProfileIds = customProfileIds;
       this.$emit('saveUser');
-      this.$emit('updateTableSettings', { profileEditor: this.editorVisible, ...this.newTableSettings });
+      this.$emit('updateTableSettings', {
+        profileEditor: this.editorVisible,
+        ...this.newTableSettings,
+      });
     },
     requestPermissions() {
       this.$emit('requestPermissions');
@@ -523,7 +602,10 @@ export default {
       });
     },
     saveActiveProfile() {
-      this.activeProfile.profile.custom!.color = this.activeProfile.profile.custom!.color.replace('#', '');
+      this.activeProfile.profile.custom!.color = this.activeProfile.profile.custom!.color.replace(
+        '#',
+        '',
+      );
       this.$emit('updateProfile', this.activeProfile);
       this.editorVisible = false;
     },
@@ -531,9 +613,11 @@ export default {
       this.$ext.log('removeIamRole');
       const iamRoles: IamRole[] = [];
       appProfile.profile.custom!.iamRoles.forEach((role) => {
-        if (role.roleName !== iamRole.roleName
+        if (
+          role.roleName !== iamRole.roleName
           || role.accountId !== iamRole.accountId
-          || role.profileId !== iamRole.profileId) {
+          || role.profileId !== iamRole.profileId
+        ) {
           iamRoles.push(role);
         }
       });
@@ -543,9 +627,7 @@ export default {
     setProfiles(profiles) {
       this.$ext.log('setProfiles');
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      profiles.forEach((profile) => {
-
-      });
+      profiles.forEach((profile) => {});
       this.$ext.log(profiles);
     },
     assumeIamRole(iamRole, appProfile) {
@@ -590,10 +672,15 @@ export default {
   overflow: hidden !important;
   text-overflow: ellipsis !important;
 }
-.table-header, .app-title, .app-subtitle {
-  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+.table-header,
+.app-title,
+.app-subtitle {
+  font-size: 12px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 }
-.filter-button, .filter {
+.filter-button,
+.filter {
   vertical-align: middle !important;
   padding-left: 10px !important;
 }
@@ -617,7 +704,7 @@ export default {
   padding-right: 10px;
   border-bottom: 1px solid #dee2e6;
 }
-.profile:hover  {
+.profile:hover {
   background-color: #f3f5fb;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 5px 5px;
 }
